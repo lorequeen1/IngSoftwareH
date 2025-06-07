613bb0625faeee68035fa6b9eea39ae4dcb05c4c
lorequeen1 <loredana.munoz45@gmail.com>
2025-06-07 15:07:18 -0500

    paso6

--- ingsoftware1/ingsoftware1/urls.py
+++ ingsoftware1/ingsoftware1/urls.py
@@ -1,8 +1,8 @@
 """
-URL configuration for ingsoftware1 project.
+URL configuration for Sistema_Hotel project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,8 +15,28 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Hotel API",
+        default_version='v1',
+        description="Documentación de la API para el sistema de reserva de habitaciones de hotel",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@hotel.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('loredanaing.urls')),
+
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redocs/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc')
 ]
